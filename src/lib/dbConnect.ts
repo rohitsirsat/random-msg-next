2cc63c910fc2ad3288d888005906d2a4ce880607
rohit <rohitsirsat855@gmail.com>
2024-05-08 18:49:55 +0530

    connect db

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -15,6 +15,10 @@ async function dbConnect(): Promise<void> {
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
     connection.isConnected = db.connections[0].readyState;
+    // readyState ==> is a num which we extract
+
+    //TODO: log => db and db.connections
+
     console.log("DB Connected Successfully");
   } catch (error) {
     console.log("Database connection failed", error);

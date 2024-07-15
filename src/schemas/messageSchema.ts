194b36e0f411e98702b146f15408d31303657e3b
rohit <rohitsirsat855@gmail.com>
2024-07-15 12:15:37 +0530

    restart the project

--- src/schemas/messageSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const messageSchema = z.object({
-  content: z
-    .string()
-    .min(10, { message: "content must be at least 10 characters" })
-    .max(300, { message: "Content must be no longer than 300 characters " }),
-});
--- src/schemas/signInSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-// indentifier ==> we can call it as username or email (identifier is better word used in production)
-export const signInSchema = z.object({
-  identifier: z.string(),
-  password: z.string(),
-});
